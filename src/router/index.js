e7e0eeee3be7e2578e112c51b70b5902f5403947
dengshilong <dengshilong1988@gmail.com>
2018-07-12 17:52:57 +0800

    fix: change router mode to history

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Posts from '@/components/Posts'
+import NotFoundComponent from '@/components/404'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,6 +18,7 @@ export default new Router({
       path: '/posts',
       name: 'posts',
       component: Posts
-    }
+    },
+    { path: '*', component: NotFoundComponent }
   ]
 })
