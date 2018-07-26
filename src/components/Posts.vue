297071baa791982e77ea9c5146e0b9d7d6170f60
dengshilong <dengshilong1988@gmail.com>
2018-07-26 21:48:48 +0800

    fix: add pagination

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -2,6 +2,15 @@
   <div>
     <div v-if="posts.length">
       <blog-post v-for="post in posts" :key="post.id" :post="post"></blog-post>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[1, 2, 3, 5, 10, 20, 100]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </div>
     <div v-else><h2>No Posts</h2></div>
   </div>
@@ -21,6 +30,9 @@ export default {
     return {
       posts: [],
       params: {},
+      total: 0,
+      currentPage: 1,
+      pageSize: 10,
     }
   },
   created() {
@@ -30,9 +42,16 @@ export default {
     fetchPost() {
       let params = this.$route.params
       lodash.extend(params, this.$route.query)
+      if (this.$route.query['page']) {
+        this.currentPage = this.$route.query['page']
+        this.$route.query['page'] = null
+      }
+      params['page'] = this.currentPage
+      params['page_size'] = this.pageSize
       axios.get('http://127.0.0.1:7001/api/posts/', {params: params})
         .then(response => {
           this.posts = response.data.results
+          this.total = response.data.count
         })
         .catch(error => {
           if (err.response.status == 404) {
@@ -42,7 +61,14 @@ export default {
           console.log(error);
         })
     },
-    
-  }
+    handleCurrentChange(page) {
+      this.currentPage = page
+      this.fetchPost()
+    },
+    handleSizeChange(size) {
+      this.pageSize = size
+      this.fetchPost()
+    }
+  },
 }
 </script>
